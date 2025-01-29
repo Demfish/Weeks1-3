e76f56b35212e402d4741a4657574f9efbb7eae1
DESKTOP-0LA1SBT\mitch <demczyna@shernet.sheridancollege.ca>
2025-01-29 15:49:47 -0500

    added flying ship

--- Assets/Scenes/Mechanical Drawing/Lerping.cs
+++ Assets/Scenes/Mechanical Drawing/Lerping.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class LerpDemo : MonoBehaviour
+public class Lerping : MonoBehaviour
 {
     public AnimationCurve curve;
 
     [Range(0, 1)]
-    public float t;
+    float t = 0f;
     public float speed = 1f;
     public Transform start;
     public Transform end;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,7 @@ public class LerpDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+        t += Time.deltaTime * speed;
+        transform.position = Vector3.Lerp(start.position, end.position, curve.Evaluate(t));
     }
 }
